9c4132ccd59d97735d6f138447088c9f3205c726
shibukazu <61775791+shibukazu@users.noreply.github.com>
2024-07-06 11:41:13 +0900

    feat: additional type support (#7)

--- go/pkg/services/validate/v1/check.go
+++ go/pkg/services/validate/v1/check.go
@@ -39,12 +39,78 @@ func convertAnyMapToInterfaceMap(anyMap map[string]*anypb.Any) (map[string]inter
 				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
 			}
 			val = stringValue.Value
+		case "type.googleapis.com/google.protobuf.DoubleValue":
+			doubleValue := &wrapperspb.DoubleValue{}
+			if err := anyValue.UnmarshalTo(doubleValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = doubleValue.Value
+		case "type.googleapis.com/google.protobuf.FloatValue":
+			floatValue := &wrapperspb.FloatValue{}
+			if err := anyValue.UnmarshalTo(floatValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = floatValue.Value
 		case "type.googleapis.com/google.protobuf.Int32Value":
 			intValue := &wrapperspb.Int32Value{}
 			if err := anyValue.UnmarshalTo(intValue); err != nil {
 				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
 			}
 			val = intValue.Value
+		case "type.googleapis.com/google.protobuf.Int64Value":
+			intValue := &wrapperspb.Int64Value{}
+			if err := anyValue.UnmarshalTo(intValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = intValue.Value
+		case "type.googleapis.com/google.protobuf.UInt32Value":
+			uintValue := &wrapperspb.UInt32Value{}
+			if err := anyValue.UnmarshalTo(uintValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = uintValue.Value
+		case "type.googleapis.com/google.protobuf.UInt64Value":
+			uintValue := &wrapperspb.UInt64Value{}
+			if err := anyValue.UnmarshalTo(uintValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = uintValue.Value
+		case "type.googleapis.com/google.protobuf.SInt32Value":
+			intValue := &wrapperspb.Int32Value{}
+			if err := anyValue.UnmarshalTo(intValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = intValue.Value
+		case "type.googleapis.com/google.protobuf.SInt64Value":
+			intValue := &wrapperspb.Int64Value{}
+			if err := anyValue.UnmarshalTo(intValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = intValue.Value
+		case "type.googleapis.com/google.protobuf.Fixed32Value":
+			uintValue := &wrapperspb.UInt32Value{}
+			if err := anyValue.UnmarshalTo(uintValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = uintValue.Value
+		case "type.googleapis.com/google.protobuf.Fixed64Value":
+			uintValue := &wrapperspb.UInt64Value{}
+			if err := anyValue.UnmarshalTo(uintValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = uintValue.Value
+		case "type.googleapis.com/google.protobuf.BoolValue":
+			boolValue := &wrapperspb.BoolValue{}
+			if err := anyValue.UnmarshalTo(boolValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = boolValue.Value
+		case "type.googleapis.com/google.protobuf.BytesValue":
+			bytesValue := &wrapperspb.BytesValue{}
+			if err := anyValue.UnmarshalTo(bytesValue); err != nil {
+				return nil, failure.Translate(err, appError.ErrRequestParameterInvalid)
+			}
+			val = bytesValue
 		default:
 			return nil, failure.New(appError.ErrRequestParameterInvalid, failure.Messagef("unsupported type: %s", anyValue.TypeUrl))
 		}
